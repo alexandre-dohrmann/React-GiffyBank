d2ce5681c043b414e577b75b52a75e4d2290e5a7
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-08-07 16:53:33 -0600

    Updated Search for GIFs

--- giffy-bank-react/src/components/SearchBar.jsx
+++ giffy-bank-react/src/components/SearchBar.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const SearchBar = (props) => {
 
     return (
-        <div className="search m-2">
-            <input type="text" name="search" onChange={props.handleTermChange} />
+        <div>
+            <div className="form-group log-in-box">
+                <input className="form-control search-input" type="text" name="search" onChange={props.handleTermChange} placeholder="search giphy database" />
+            </div>
         </div>
     );
 }
